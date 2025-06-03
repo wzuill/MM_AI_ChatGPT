dd409a92fdc399558ee9083cb6540bb0936eefe9
woody <woody@woodyzuill.com>
2025-06-03 06:48:20 -0700

    RuleLoader now uses MarkdownRuleParser

--- src/RuleLoader.java
+++ src/RuleLoader.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class RuleLoader {
@@ -7,22 +6,51 @@ public class RuleLoader {
         TWO_COLOR, THREE_COLOR, FOUR_COLOR
     }
 
-    private final Map<RuleKey, DeductionRule> rules;
+    public static class RuleKey {
+        private final GuessPatternType patternType;
+        private final Feedback feedback;
+
+        public RuleKey(GuessPatternType patternType, Feedback feedback) {
+            this.patternType = patternType;
+            this.feedback = feedback;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof RuleKey)) return false;
+            RuleKey other = (RuleKey) obj;
+            return patternType == other.patternType && Objects.equals(feedback, other.feedback);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(patternType, feedback);
+        }
+    }
+
+    private final Map<RuleKey, DeductionRule> rules = new HashMap<>();
 
     public RuleLoader() {
-        rules = new HashMap<>();
         registerRules();
     }
+
+    private void registerRules() {
+        MarkdownRuleParser.registerRulesInto(rules);
+    }
+
     public GuessPatternType classify(Guess guess) {
         Set<Integer> distinct = new HashSet<>();
         for (int value : guess.values()) {
             distinct.add(value);
         }
 
-        int size = distinct.size();
-        if (size == 2) return GuessPatternType.TWO_COLOR;
-        else if (size == 3) return GuessPatternType.THREE_COLOR;
-        else return GuessPatternType.FOUR_COLOR;
+        if (distinct.size() == 2)
+            return GuessPatternType.TWO_COLOR;
+        else if (distinct.size() == 3)
+            return GuessPatternType.THREE_COLOR;
+        else
+            return GuessPatternType.FOUR_COLOR;
     }
 
 
@@ -30,41 +58,9 @@ public class RuleLoader {
         GuessPatternType type = classify(guess);
         RuleKey key = new RuleKey(type, feedback);
         DeductionRule rule = rules.get(key);
-        if (rule == null) throw new IllegalArgumentException("No rule for: " + key);
+        if (rule == null) {
+            throw new IllegalArgumentException("No rule found for pattern type: " + type + ", feedback: " + feedback);
+        }
         return rule;
     }
-
-    private void registerRules() {
-        rules.put(new RuleKey(GuessPatternType.TWO_COLOR, new Feedback(0, 0)), CanonicalMatrixRule.for2Color_0_0());
-
-        // Add other rules here later...
-    }
-
-    private static class RuleKey {
-        private final GuessPatternType type;
-        private final Feedback feedback;
-
-        RuleKey(GuessPatternType type, Feedback feedback) {
-            this.type = type;
-            this.feedback = feedback;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof RuleKey)) return false;
-            RuleKey rk = (RuleKey) o;
-            return type == rk.type && feedback.equals(rk.feedback);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(type, feedback);
-        }
-
-        @Override
-        public String toString() {
-            return type + " + " + feedback;
-        }
-    }
 }
-
